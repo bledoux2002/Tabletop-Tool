a40ab1f02bea3c7aff9d5d1d43586b59c2889ca3
Benjamin Ledoux <bledoux2002@gmail.com>
2026-07-06 18:02:01 -0500

    extracted all tables to csv, '?' for non-standard characters

--- pdf/extract.py
+++ pdf/extract.py
@@ -1,25 +1,26 @@
 import pdfplumber as pp
 import pandas as pd
+import json
 
 
-def main():
-    
+def small_guns():
     with pp.open("./files/small_guns.pdf") as pdf:
         table = pdf.pages[0].extract_table()
         df = pd.DataFrame(table[1:], columns=table[0])
         print(df)
-    return
-    with pp.open("./files/small_guns.pdf") as pdf:
-        # 1. Extract raw characters
-        chars = pdf.pages[0].chars
         
-        # 2. Filter to keep only ASCII or standard Unicode characters 
-        # (Using .isprintable() or a regex)
-        filtered_chars = [c['text'] for c in chars if c['text'].isprintable()]
-        
-        # 3. Reconstruct the text
-        clean_text = "".join(filtered_chars)
-        print(clean_text)
+def main():
+    with pp.open("./files/equipment.pdf") as pdf:
+        for i, page in enumerate(pdf.pages):
+            tables = page.extract_tables()
+            for table in tables:
+                df = pd.DataFrame(table[1:], columns=table[0])
+                df = df.replace(r'\n', ' ', regex=True)
+                df.columns = df.columns.str.replace(r'\n', ' ', regex=True)
+                df.to_csv(f'./files/equipment_{i}.csv', encoding='ascii', errors='replace', index=False)
+                # df = df.fillna('')
+
+                print(df)
 
 if __name__ == "__main__":
     main()
